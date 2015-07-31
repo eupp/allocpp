8a8b07523db0ea969a9cc50e3cdab089f396a02d
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2015-07-31 18:01:40 +0300

    chunk refactoring

--- allocator/pool_allocation.hpp
+++ allocator/pool_allocation.hpp
@@ -6,6 +6,7 @@
 #include <limits>
 #include <vector>
 #include <memory>
+#include <type_traits>
 
 #include "alloc_traits.hpp"
 #include "alloc_policies.hpp"
@@ -18,32 +19,31 @@ namespace alloc_utility
 namespace details
 {
 
-template <typename T, typename alloc_traits = allocation_traits<T>>
+template <typename pointer, typename size_type>
 class chunk
 {
 public:
 
-    DECLARE_ALLOC_TRAITS(T, alloc_traits)
+    static_assert(std::is_same<typename std::pointer_traits<pointer>::element_type, std::uint8_t>::value,
+                  "Type of pointed value should be uint8_t");
 
-    typedef rebind_pointer<std::uint8_t> raw_pointer;
+    static const int CHUNK_MAXSIZE = std::numeric_limits<std::uint8_t>::max();
 
-    static const int CHUNK_SIZE = std::numeric_limits<std::uint8_t>::max();
-
-    chunk()
+    explicit chunk(size_type obj_size, std::uint8_t chunk_size = CHUNK_MAXSIZE)
     {
-        set_pointer(pointer(nullptr));
+        set_pointer(pointer(nullptr), obj_size, chunk_size);
     }
 
-    explicit chunk(const pointer& chunk_ptr, bool delete_flag = true)
+    chunk(const pointer& chunk_ptr, size_type obj_size, std::uint8_t chunk_size = CHUNK_MAXSIZE)
     {
-        set_pointer(chunk_ptr, delete_flag);
+        set_pointer(chunk_ptr, obj_size, chunk_size);
     }
 
     chunk(chunk&& other) noexcept:
         m_chunk(other.m_chunk)
       , m_head(other.m_head)
       , m_available(other.m_available)
-      , m_delete_flag(other.m_delete_flag)
+      , m_size(other.m_size)
     {}
 
     chunk(const chunk&) = delete;
@@ -55,18 +55,17 @@ public:
         return m_chunk;
     }
 
-    void set_pointer(pointer ptr, bool delete_flag = true)
+    void set_pointer(pointer ptr, size_type obj_size, std::uint8_t chunk_size = CHUNK_MAXSIZE)
     {
         m_chunk = ptr;
         m_head = 0;
-        m_available = CHUNK_SIZE;
-        m_delete_flag = delete_flag;
+        m_available = chunk_size;
+        m_size = chunk_size;
         if (ptr) {
-            for (int i = 1; i <= CHUNK_SIZE; ++i, ++ptr) {
-                raw_pointer bytes = pointer_cast_traits<raw_pointer, pointer>::reinterpet_pointer_cast(ptr);
-                bytes[0] = i;
+            for (int i = 1; i <= chunk_size; ++i, ptr += obj_size) {
+                ptr[0] = i;
             }
-            // last block is initialized with CHUNK_SIZE
+            // last block is initialized with chunk_size
             // it is really doesn't matter because when we reach last block m_available == 0
         }
     }
@@ -76,188 +75,198 @@ public:
         return m_chunk && (m_available > 0);
     }
 
-    bool is_owned(const pointer& ptr) const noexcept
+    bool is_owned(const pointer& ptr, size_type obj_size) const noexcept
     {
-        return m_chunk && (m_chunk <= ptr) && (ptr < m_chunk + CHUNK_SIZE);
+        return m_chunk && (m_chunk <= ptr) && (ptr < m_chunk + size() * obj_size);
     }
 
-    bool delete_flag() const noexcept
+    std::uint8_t size() const noexcept
     {
-        return m_delete_flag;
+        return m_size;
     }
 
-    pointer allocate()
+    pointer allocate(size_type obj_size)
     {
         if (is_memory_available()) {
-            pointer ptr = m_chunk + m_head;
-            raw_pointer bytes = pointer_cast_traits<raw_pointer, pointer>::reinterpet_pointer_cast(ptr);
-            m_head = bytes[0];
+            pointer ptr = m_chunk + (m_head * obj_size);
+            m_head = ptr[0];
             m_available--;
             return ptr;
         }
         return pointer(nullptr);
     }
 
-    void deallocate(const pointer& ptr)
+    void deallocate(const pointer& ptr, size_type obj_size)
     {
-        difference_type diff = ptr - m_chunk;
-        raw_pointer byte = pointer_cast_traits<raw_pointer, pointer>::reinterpet_pointer_cast(ptr);
-        byte[0] = m_head;
-        m_head = diff;
+        ptr[0] = m_head;
+        m_head = (ptr - m_chunk) / obj_size;
         m_available++;
     }
 
 private:
-
     pointer m_chunk;
     std::uint8_t m_head;
     std::uint8_t m_available;
-    bool m_delete_flag;
+    std::uint8_t m_size;
 };
 
-template <typename T, typename allocator, typename alloc_traits = allocation_traits<T>>
-class memory_pool
-{
-public:
+}
 
-    DECLARE_ALLOC_TRAITS(T, alloc_traits)
+//template <typename T, typename allocator, typename alloc_traits = allocation_traits<T>>
+//class memory_pool
+//{
+//public:
 
-    static const int CHUNK_SIZE = chunk<T, alloc_traits>::CHUNK_SIZE;
+//    DECLARE_ALLOC_TRAITS(T, alloc_traits)
 
-    memory_pool(allocator* alloc) noexcept:
-        m_alloc(alloc)
-    {
-        assert(alloc);
-    }
+//    static const int CHUNK_SIZE = chunk<T, alloc_traits>::CHUNK_MAXSIZE;
 
-    ~memory_pool()
-    {
-        for (auto& chunk: m_pool) {
-            if (chunk.delete_flag()) {
-                m_alloc->deallocate(chunk.get_pointer(), CHUNK_SIZE);
-            }
-        }
-    }
+//    memory_pool(allocator* alloc) noexcept:
+//        m_alloc(alloc)
+//    {
+//        assert(alloc);
+//    }
 
-    memory_pool(const memory_pool&) = delete;
-    memory_pool(memory_pool&&) = delete;
-    memory_pool& operator=(const memory_pool&) = delete;
-    memory_pool& operator=(memory_pool&&) = delete;
+//    ~memory_pool()
+//    {
+//        for (auto& chunk: m_pool) {
+//            if (chunk.delete_flag()) {
+//                m_alloc->deallocate(chunk.get_pointer(), CHUNK_SIZE);
+//            }
+//        }
+//    }
 
-    size_type size() const noexcept
-    {
-        return CHUNK_SIZE * m_pool.size();
-    }
+//    memory_pool(const memory_pool&) = delete;
+//    memory_pool(memory_pool&&) = delete;
+//    memory_pool& operator=(const memory_pool&) = delete;
+//    memory_pool& operator=(memory_pool&&) = delete;
 
-    void reserve(size_type new_size)
-    {
-        if (new_size <= size()) {
-            return;
-        }
-        int old_chunks_count = m_pool.size();
-        int chunks_count = (new_size + CHUNK_SIZE - 1) / CHUNK_SIZE;
-        int chunks_diff = chunks_count - old_chunks_count;
-        m_pool.resize(chunks_count);
-        pointer ptr = m_alloc->allocate(chunks_diff * CHUNK_SIZE, pointer(nullptr));
+//    size_type size() const noexcept
+//    {
+//        return CHUNK_SIZE * m_pool.size();
+//    }
 
-        int i = old_chunks_count;
-        m_pool[i].set_pointer(ptr, true);
-        ++i;
-        ptr += CHUNK_SIZE;
-        for (; i < chunks_count; ++i, ptr += CHUNK_SIZE) {
-            m_pool[i].set_pointer(ptr, false);
-        }
-    }
+//    void reserve(size_type new_size)
+//    {
+//        if (new_size <= size()) {
+//            return;
+//        }
+//        int old_chunks_count = m_pool.size();
+//        int chunks_count = (new_size + CHUNK_SIZE - 1) / CHUNK_SIZE;
+//        int chunks_diff = chunks_count - old_chunks_count;
+//        m_pool.resize(chunks_count);
+//        pointer ptr = m_alloc->allocate(chunks_diff * CHUNK_SIZE, pointer(nullptr));
 
-    pointer allocate(const const_void_pointer& hint = nullptr)
-    {
-        ALLOC_UNUSED(hint);
-        for (auto& chunk: m_pool) {
-            if (chunk.is_memory_available()) {
-                return chunk.allocate();
-            }
-        }
-        m_pool.emplace_back(m_alloc->allocate(CHUNK_SIZE, pointer(nullptr)));
-        return m_pool.back().allocate();
-    }
+//        int i = old_chunks_count;
+//        m_pool[i].set_pointer(ptr, true);
+//        ++i;
+//        ptr += CHUNK_SIZE;
+//        for (; i < chunks_count; ++i, ptr += CHUNK_SIZE) {
+//            m_pool[i].set_pointer(ptr, false);
+//        }
+//    }
 
-    void deallocate(const pointer& ptr)
-    {
-        for (auto& chunk: m_pool) {
-            if (chunk.is_owned(ptr)) {
-                chunk.deallocate(ptr);
-            }
-        }
-    }
+//    pointer allocate(const const_void_pointer& hint = nullptr)
+//    {
+//        ALLOC_UNUSED(hint);
+//        for (auto& chunk: m_pool) {
+//            if (chunk.is_memory_available()) {
+//                return chunk.allocate();
+//            }
+//        }
+//        m_pool.emplace_back(m_alloc->allocate(CHUNK_SIZE, pointer(nullptr)));
+//        return m_pool.back().allocate();
+//    }
 
-private:
-    std::vector<details::chunk<T, alloc_traits>> m_pool;
-    allocator* m_alloc;
-};
+//    void deallocate(const pointer& ptr)
+//    {
+//        for (auto& chunk: m_pool) {
+//            if (chunk.is_owned(ptr)) {
+//                chunk.deallocate(ptr);
+//            }
+//        }
+//    }
 
-} // namespace details
+//private:
+//    std::vector<details::chunk<T, alloc_traits>> m_pool;
+//    allocator* m_alloc;
+//};
 
-template <typename T, typename alloc_traits = allocation_traits<T>,
-          typename base_policy = default_allocation_policy<T, alloc_traits>>
-class pool_allocation_policy: public base_policy
-{
-    typedef details::memory_pool<T, base_policy, alloc_traits> pool_type;
+//} // namespace details
 
-public:
+//template <typename T, typename alloc_traits = allocation_traits<T>,
+//          typename base_policy = default_allocation_policy<T, alloc_traits>>
+//class pool_allocation_policy: public base_policy
+//{
+//    typedef details::memory_pool<T, base_policy, alloc_traits> pool_type;
 
-    DECLARE_ALLOC_TRAITS(T, alloc_traits)
-    DECLARE_REBIND_ALLOC(pool_allocation_policy, T, alloc_traits, base_policy)
+//public:
 
-    explicit pool_allocation_policy():
-        m_pool(std::make_shared<pool_type>(this))
-    {}
+//    DECLARE_ALLOC_TRAITS(T, alloc_traits)
+//    DECLARE_REBIND_ALLOC(pool_allocation_policy, T, alloc_traits, base_policy)
 
-    pool_allocation_policy(const pool_allocation_policy& other):
-        base_policy(other)
-      , m_pool(other.m_pool)
-    {}
+//    explicit pool_allocation_policy():
+//        m_pool(std::make_shared<pool_type>(this))
+//    {}
 
-    template <typename U>
-    pool_allocation_policy(const pool_allocation_policy::rebind<U>& other):
-        base_policy(other)
-      , m_pool(std::make_shared<pool_type>(this))
-    {}
+//    pool_allocation_policy(const pool_allocation_policy& other):
+//        base_policy(other)
+//      , m_pool(other.m_pool)
+//    {}
 
-    pool_allocation_policy& operator=(const pool_allocation_policy&) = delete;
-    pool_allocation_policy& operator=(pool_allocation_policy&&) = delete;
+//    template <typename U>
+//    pool_allocation_policy(const pool_allocation_policy::rebind<U>& other):
+//        base_policy(other)
+//      , m_pool(std::make_shared<pool_type>(this))
+//    {}
 
-    size_type size() const noexcept
-    {
-        return m_pool->size();
-    }
+//    pool_allocation_policy& operator=(const pool_allocation_policy&) = delete;
+//    pool_allocation_policy& operator=(pool_allocation_policy&&) = delete;
 
-    void reserve(size_type new_size)
-    {
-        m_pool->reserve(new_size);
-    }
+//    size_type size() const noexcept
+//    {
+//        return m_pool->size();
+//    }
 
-    pointer allocate(size_type n, const pointer& ptr, const const_void_pointer& hint = nullptr)
-    {
-        if (ptr) {
-            return ptr;
-        }
-        if (n > 1) {
-            return base_policy::allocate(n, ptr, hint);
-        }
-        return m_pool->allocate(hint);
-    }
+//    void reserve(size_type new_size)
+//    {
+//        m_pool->reserve(new_size);
+//    }
 
-    void deallocate(const pointer& ptr, size_type n)
-    {
-        if (n > 1) {
-            base_policy::deallocate(ptr, n);
-        }
-        m_pool->deallocate(ptr);
-    }
+//    pointer allocate(size_type n, const pointer& ptr, const const_void_pointer& hint = nullptr)
+//    {
+//        if (ptr) {
+//            return ptr;
+//        }
+//        if (n > 1) {
+//            return base_policy::allocate(n, ptr, hint);
+//        }
+//        return m_pool->allocate(hint);
+//    }
 
-private:
-    std::shared_ptr<pool_type> m_pool;
-};
+//    void deallocate(const pointer& ptr, size_type n)
+//    {
+//        if (n > 1) {
+//            base_policy::deallocate(ptr, n);
+//        }
+//        m_pool->deallocate(ptr);
+//    }
+
+////    template <typename U>
+////    bool operator==(const pool_allocation_policy::rebind<U>& other)
+////    {
+////        return base_policy::operator==(other);
+////    }
+
+////    template <typename U>
+////    bool operator!=(const pool_allocation_policy::rebind<U>& other)
+////    {
+////        return base_policy::operator!=(other);
+////    }
+
+//private:
+//    std::shared_ptr<pool_type> m_pool;
+//};
 
 } // namespace alloc_utility
 
--- test/pool_test.cpp
+++ test/pool_test.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <random>
 #include <functional>
 #include <stack>
@@ -35,11 +36,11 @@ class chunk_test: public ::testing::Test
 {
 public:
 
-    static const int CHUNK_SIZE = chunk<int>::CHUNK_SIZE;
+    static const int CHUNK_SIZE = chunk<std::uint8_t*, size_t>::CHUNK_MAXSIZE;
 
     chunk_test():
-        mem(new int[CHUNK_SIZE])
-      , chk(mem)
+        mem(new std::uint8_t[sizeof(int) * CHUNK_SIZE])
+      , chk(mem, sizeof(int))
       , rand(0.75)
     {}
 
@@ -48,266 +49,271 @@ public:
         delete[] mem;
     }
 
-    int* mem;
-    chunk<int> chk;
+    std::uint8_t* mem;
+    chunk<std::uint8_t*, size_t> chk;
     bernulli_generator rand;
 };
+
+TEST_F(chunk_test, test_size)
+{
+    EXPECT_EQ((int)CHUNK_SIZE, chk.size());
+}
 
 TEST_F(chunk_test, test_is_memory_available)
 {
     EXPECT_TRUE(chk.is_memory_available());
 
-    for (int i = 0; i < chk.CHUNK_SIZE; ++i) {
-        chk.allocate();
+    for (int i = 0; i < CHUNK_SIZE; ++i) {
+        chk.allocate(sizeof(int));
     }
     EXPECT_FALSE(chk.is_memory_available());
 
-    chk.set_pointer(nullptr);
+    chk.set_pointer(nullptr, sizeof(int));
     EXPECT_FALSE(chk.is_memory_available());
 }
 
 TEST_F(chunk_test, test_is_owned)
 {
-    EXPECT_TRUE(chk.is_owned(mem));
-    EXPECT_FALSE(chk.is_owned(mem + CHUNK_SIZE + 10));
-    EXPECT_FALSE(chk.is_owned(mem - 1));
-    EXPECT_FALSE(chk.is_owned(nullptr));
+    EXPECT_TRUE(chk.is_owned(mem, sizeof(int)));
+    EXPECT_FALSE(chk.is_owned(mem + (CHUNK_SIZE + 10) * sizeof(int), sizeof(int)));
+    EXPECT_FALSE(chk.is_owned(mem - 1, sizeof(int)));
+    EXPECT_FALSE(chk.is_owned(nullptr, sizeof(int)));
 }
 
 TEST_F(chunk_test, test_allocate)
 {
-    int* ptr1 = chk.allocate();
+    std::uint8_t* ptr1 = chk.allocate(sizeof(int));
     EXPECT_NE(nullptr, ptr1);
-    EXPECT_TRUE(chk.is_owned(ptr1));
+    EXPECT_TRUE(chk.is_owned(ptr1, sizeof(int)));
     // try dereference pointer
     // in case of incorrect allocation it might cause segmentation fault
     *ptr1 = 42;
 
-    int* ptr2 = chk.allocate();
+    std::uint8_t* ptr2 = chk.allocate(sizeof(int));
     EXPECT_NE(nullptr, ptr2);
     EXPECT_NE(ptr1, ptr2);
-    EXPECT_TRUE(chk.is_owned(ptr2));
+    EXPECT_TRUE(chk.is_owned(ptr2, sizeof(int)));
     *ptr2 = 42;
 
     for (int i = 0; i < CHUNK_SIZE - 2; ++i) {
-        int* ptr3 = chk.allocate();
+        std::uint8_t* ptr3 = chk.allocate(sizeof(int));
         *ptr3 = 42;
     }
 
-    int* ptr4 = chk.allocate();
+    std::uint8_t* ptr4 = chk.allocate(sizeof(int));
     EXPECT_EQ(nullptr, ptr4);
 }
 
 TEST_F(chunk_test, test_deallocate)
 {
-    int* ptr = nullptr;
+    std::uint8_t* ptr = nullptr;
     for (int i = 0; i < CHUNK_SIZE; ++i) {
-        ptr = chk.allocate();
+        ptr = chk.allocate(sizeof(int));
     }
-    chk.deallocate(ptr);
+    chk.deallocate(ptr, sizeof(int));
     EXPECT_TRUE(chk.is_memory_available());
 
     ptr = nullptr;
-    ptr = chk.allocate();
+    ptr = chk.allocate(sizeof(int));
     EXPECT_NE(nullptr, ptr);
 }
 
 TEST_F(chunk_test, test_stress)
 {
     const int ITER_COUNT = 2000;
-    std::stack<int*> st;
+    std::stack<std::uint8_t*> st;
     for (int i = 0; i < ITER_COUNT; ++i) {
         if (rand() && chk.is_memory_available()) {
-            int* ptr = chk.allocate();
+            std::uint8_t* ptr = chk.allocate(sizeof(int));
             ASSERT_NE(nullptr, ptr);
             // try dereference pointer
             // in case of incorrect allocation it might cause segmentation fault
             *ptr = 42;
             st.push(ptr);
         } else if (!st.empty()) {
-            int* ptr = st.top();
+            std::uint8_t* ptr = st.top();
             st.pop();
-            ASSERT_TRUE(chk.is_owned(ptr));
-            chk.deallocate(ptr);
+            ASSERT_TRUE(chk.is_owned(ptr, sizeof(int)));
+            chk.deallocate(ptr, sizeof(int));
             ASSERT_TRUE(chk.is_memory_available());
         }
     }
 }
 
-class memory_pool_test: public ::testing::Test
-{
-public:
+//class memory_pool_test: public ::testing::Test
+//{
+//public:
 
-    typedef default_allocation_policy<int, allocation_traits<int>, statistic_policy<int>> alloc_policy;
-    typedef details::memory_pool<int, alloc_policy> pool_type;
-    typedef typename alloc_policy::statistic_type statistic;
+//    typedef default_allocation_policy<int, allocation_traits<int>, statistic_policy<int>> alloc_policy;
+//    typedef details::memory_pool<int, alloc_policy> pool_type;
+//    typedef typename alloc_policy::statistic_type statistic;
 
-    static const int CHUNK_SIZE = pool_type::CHUNK_SIZE;
+//    static const int CHUNK_SIZE = pool_type::CHUNK_SIZE;
 
-    memory_pool_test():
-        pool(&alloc)
-      , rand(0.75)
-    {
-        alloc.set_statistic(&stat);
-    }
+//    memory_pool_test():
+//        pool(&alloc)
+//      , rand(0.75)
+//    {
+//        alloc.set_statistic(&stat);
+//    }
 
-    statistic stat;
-    alloc_policy alloc;
-    pool_type pool;
-    bernulli_generator rand;
-};
+//    statistic stat;
+//    alloc_policy alloc;
+//    pool_type pool;
+//    bernulli_generator rand;
+//};
 
-TEST_F(memory_pool_test, test_allocate)
-{
-    int* ptr1 = pool.allocate();
-    EXPECT_NE(nullptr, ptr1);
-    // try dereference pointer
-    // in case of incorrect allocation it might cause segmentation fault
-    *ptr1 = 42;
-    EXPECT_EQ(1, stat.allocs_count());
-    EXPECT_EQ(1, stat.allocated_blocks_count());
-    EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
+//TEST_F(memory_pool_test, test_allocate)
+//{
+//    int* ptr1 = pool.allocate();
+//    EXPECT_NE(nullptr, ptr1);
+//    // try dereference pointer
+//    // in case of incorrect allocation it might cause segmentation fault
+//    *ptr1 = 42;
+//    EXPECT_EQ(1, stat.allocs_count());
+//    EXPECT_EQ(1, stat.allocated_blocks_count());
+//    EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
 
-    for (int i = 0; i < CHUNK_SIZE - 1; ++i) {
-        int* ptr2 = pool.allocate();
-        *ptr2 = 42;
-        EXPECT_EQ(1, stat.allocs_count());
-        EXPECT_EQ(1, stat.allocated_blocks_count());
-        EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
-    }
+//    for (int i = 0; i < CHUNK_SIZE - 1; ++i) {
+//        int* ptr2 = pool.allocate();
+//        *ptr2 = 42;
+//        EXPECT_EQ(1, stat.allocs_count());
+//        EXPECT_EQ(1, stat.allocated_blocks_count());
+//        EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
+//    }
 
-    int* ptr3 = pool.allocate();
-    EXPECT_NE(nullptr, ptr3);
-    EXPECT_NE(ptr1, ptr3);
-    *ptr3 = 42;
-    EXPECT_EQ(2, stat.allocs_count());
-    EXPECT_EQ(2, stat.allocated_blocks_count());
-    EXPECT_EQ(2 * CHUNK_SIZE * sizeof(int), stat.mem_used());
-}
+//    int* ptr3 = pool.allocate();
+//    EXPECT_NE(nullptr, ptr3);
+//    EXPECT_NE(ptr1, ptr3);
+//    *ptr3 = 42;
+//    EXPECT_EQ(2, stat.allocs_count());
+//    EXPECT_EQ(2, stat.allocated_blocks_count());
+//    EXPECT_EQ(2 * CHUNK_SIZE * sizeof(int), stat.mem_used());
+//}
 
-TEST_F(memory_pool_test, test_deallocate)
-{
-    int* ptr1 = pool.allocate();
-    pool.deallocate(ptr1);
-    EXPECT_EQ(0, stat.deallocs_count());
-    EXPECT_EQ(1, stat.allocated_blocks_count());
-    EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
-}
+//TEST_F(memory_pool_test, test_deallocate)
+//{
+//    int* ptr1 = pool.allocate();
+//    pool.deallocate(ptr1);
+//    EXPECT_EQ(0, stat.deallocs_count());
+//    EXPECT_EQ(1, stat.allocated_blocks_count());
+//    EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
+//}
 
-TEST_F(memory_pool_test, test_size)
-{
-    EXPECT_EQ(pool.size(), 0);
-    for (int i = 0; i < 4 * CHUNK_SIZE; ++i) {
-        pool.allocate();
-    }
-    pool.allocate();
-    EXPECT_EQ(5 * CHUNK_SIZE, pool.size());
-}
+//TEST_F(memory_pool_test, test_size)
+//{
+//    EXPECT_EQ(pool.size(), 0);
+//    for (int i = 0; i < 4 * CHUNK_SIZE; ++i) {
+//        pool.allocate();
+//    }
+//    pool.allocate();
+//    EXPECT_EQ(5 * CHUNK_SIZE, pool.size());
+//}
 
-TEST_F(memory_pool_test, test_reserve)
-{
-    pool.reserve(2 * CHUNK_SIZE);
-    EXPECT_EQ(2 * CHUNK_SIZE, pool.size());
-    for (int i = 0; i < 2 * CHUNK_SIZE; ++i) {
-        int* ptr = pool.allocate();
-        EXPECT_NE(nullptr, ptr);
-        *ptr = 42;
-    }
+//TEST_F(memory_pool_test, test_reserve)
+//{
+//    pool.reserve(2 * CHUNK_SIZE);
+//    EXPECT_EQ(2 * CHUNK_SIZE, pool.size());
+//    for (int i = 0; i < 2 * CHUNK_SIZE; ++i) {
+//        int* ptr = pool.allocate();
+//        EXPECT_NE(nullptr, ptr);
+//        *ptr = 42;
+//    }
 
-    pool.reserve(CHUNK_SIZE);
-    EXPECT_EQ(2 * CHUNK_SIZE, pool.size());
+//    pool.reserve(CHUNK_SIZE);
+//    EXPECT_EQ(2 * CHUNK_SIZE, pool.size());
 
-    int* ptr = pool.allocate();
-    *ptr = 42;
-    pool.reserve(5 * CHUNK_SIZE);
-    EXPECT_EQ(5 * CHUNK_SIZE, pool.size());
-    // check old memory is valid
-    EXPECT_EQ(42, *ptr);
-}
+//    int* ptr = pool.allocate();
+//    *ptr = 42;
+//    pool.reserve(5 * CHUNK_SIZE);
+//    EXPECT_EQ(5 * CHUNK_SIZE, pool.size());
+//    // check old memory is valid
+//    EXPECT_EQ(42, *ptr);
+//}
 
-TEST_F(memory_pool_test, test_stress)
-{
-    const int ITER_COUNT = 2000;
-    std::stack<int*> st;
-    for (int i = 0; i < ITER_COUNT; ++i) {
-        if (rand()) {
-            int* ptr = pool.allocate();
-            ASSERT_NE(nullptr, ptr);
-            // try dereference pointer
-            // in case of incorrect allocation it might cause segmentation fault
-            *ptr = 42;
-            st.push(ptr);
-        } else if (!st.empty()) {
-            int* ptr = st.top();
-            st.pop();
-            pool.deallocate(ptr);
-        }
-    }
-}
+//TEST_F(memory_pool_test, test_stress)
+//{
+//    const int ITER_COUNT = 2000;
+//    std::stack<int*> st;
+//    for (int i = 0; i < ITER_COUNT; ++i) {
+//        if (rand()) {
+//            int* ptr = pool.allocate();
+//            ASSERT_NE(nullptr, ptr);
+//            // try dereference pointer
+//            // in case of incorrect allocation it might cause segmentation fault
+//            *ptr = 42;
+//            st.push(ptr);
+//        } else if (!st.empty()) {
+//            int* ptr = st.top();
+//            st.pop();
+//            pool.deallocate(ptr);
+//        }
+//    }
+//}
 
-class pool_allocation_policy_test: public ::testing::Test
-{
-public:
+//class pool_allocation_policy_test: public ::testing::Test
+//{
+//public:
 
-    typedef pool_allocation_policy<int, allocation_traits<int>,
-                                        default_allocation_policy<int, allocation_traits<int>,
-                                            statistic_policy<int>
-                                        >
-                                  > allocator;
-    typedef details::memory_pool<int, allocator> pool_type;
-    typedef typename allocator::statistic_type statistic;
+//    typedef pool_allocation_policy<int, allocation_traits<int>,
+//                                        default_allocation_policy<int, allocation_traits<int>,
+//                                            statistic_policy<int>
+//                                        >
+//                                  > allocator;
+//    typedef details::memory_pool<int, allocator> pool_type;
+//    typedef typename allocator::statistic_type statistic;
 
-    static const int CHUNK_SIZE = pool_type::CHUNK_SIZE;
+//    static const int CHUNK_SIZE = pool_type::CHUNK_SIZE;
 
-    pool_allocation_policy_test()
-    {
-        alloc.set_statistic(&stat);
-    }
+//    pool_allocation_policy_test()
+//    {
+//        alloc.set_statistic(&stat);
+//    }
 
-    statistic stat;
-    allocator alloc;
-};
+//    statistic stat;
+//    allocator alloc;
+//};
 
-TEST_F(pool_allocation_policy_test, test_allocate)
-{
-    int* ptr1 = alloc.allocate(1, nullptr);
-    EXPECT_NE(nullptr, ptr1);
-    // try dereference pointer
-    // in case of incorrect allocation it might cause segmentation fault
-    *ptr1 = 42;
-    EXPECT_EQ(1, stat.allocs_count());
-    EXPECT_EQ(1, stat.allocated_blocks_count());
-    EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
+//TEST_F(pool_allocation_policy_test, test_allocate)
+//{
+//    int* ptr1 = alloc.allocate(1, nullptr);
+//    EXPECT_NE(nullptr, ptr1);
+//    // try dereference pointer
+//    // in case of incorrect allocation it might cause segmentation fault
+//    *ptr1 = 42;
+//    EXPECT_EQ(1, stat.allocs_count());
+//    EXPECT_EQ(1, stat.allocated_blocks_count());
+//    EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
 
-    int* ptr2 = alloc.allocate(2, nullptr);
-    EXPECT_NE(nullptr, ptr2);
-    ptr2[0] = ptr2[1] = 42;
-    EXPECT_EQ(2, stat.allocs_count());
-    EXPECT_EQ(2, stat.allocated_blocks_count());
-    EXPECT_LE((CHUNK_SIZE + 2) * sizeof(int), stat.mem_used());
-    alloc.deallocate(ptr2, 2);
+//    int* ptr2 = alloc.allocate(2, nullptr);
+//    EXPECT_NE(nullptr, ptr2);
+//    ptr2[0] = ptr2[1] = 42;
+//    EXPECT_EQ(2, stat.allocs_count());
+//    EXPECT_EQ(2, stat.allocated_blocks_count());
+//    EXPECT_LE((CHUNK_SIZE + 2) * sizeof(int), stat.mem_used());
+//    alloc.deallocate(ptr2, 2);
 
-    int* ptr3 = alloc.allocate(1, nullptr);
-    int* ptr4 = alloc.allocate(1, ptr3);
-    EXPECT_EQ(ptr3, ptr4);
-}
+//    int* ptr3 = alloc.allocate(1, nullptr);
+//    int* ptr4 = alloc.allocate(1, ptr3);
+//    EXPECT_EQ(ptr3, ptr4);
+//}
 
-TEST_F(pool_allocation_policy_test, test_deallocate)
-{
-    int* ptr1 = alloc.allocate(1, nullptr);
-    alloc.deallocate(ptr1, 1);
-    EXPECT_EQ(0, stat.deallocs_count());
-    EXPECT_EQ(1, stat.allocated_blocks_count());
-    EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
+//TEST_F(pool_allocation_policy_test, test_deallocate)
+//{
+//    int* ptr1 = alloc.allocate(1, nullptr);
+//    alloc.deallocate(ptr1, 1);
+//    EXPECT_EQ(0, stat.deallocs_count());
+//    EXPECT_EQ(1, stat.allocated_blocks_count());
+//    EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
 
-    int* ptr2 = alloc.allocate(2, nullptr);
-    alloc.deallocate(ptr2, 2);
-    EXPECT_EQ(1, stat.deallocs_count());
-    EXPECT_EQ(1, stat.allocated_blocks_count());
-    EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
+//    int* ptr2 = alloc.allocate(2, nullptr);
+//    alloc.deallocate(ptr2, 2);
+//    EXPECT_EQ(1, stat.deallocs_count());
+//    EXPECT_EQ(1, stat.allocated_blocks_count());
+//    EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
 
-    alloc.deallocate(nullptr, 1);
-    EXPECT_EQ(1, stat.deallocs_count());
-    EXPECT_EQ(1, stat.allocated_blocks_count());
-    EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
-}
+//    alloc.deallocate(nullptr, 1);
+//    EXPECT_EQ(1, stat.deallocs_count());
+//    EXPECT_EQ(1, stat.allocated_blocks_count());
+//    EXPECT_EQ(CHUNK_SIZE * sizeof(int), stat.mem_used());
+//}
